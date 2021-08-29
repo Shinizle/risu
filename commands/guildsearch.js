e896ff9b87a47a12dde2906544e1ba8c0eda5ef4
Irvan Kharisma Fihan <irvankharismafihan@gmail.com>
2021-08-29 18:57:29 +0700

    add scrapping crew info

---
+++ commands/guildsearch.js
@@ -0,0 +1,53 @@
+const Discord = require('discord.js')
+
+module.exports = {
+    name: "crew",
+    description: "Guild Search",
+
+    async run (client, msg, args) {
+        if (args.length === 2 && args[0] === "search" && args[1] !== null) {
+            var request = require('request');
+
+            var headers = {
+                'Content-Type': 'application/json',
+                'Cookie': 'JSESSIONID=B5986A807D4E1FD226DC83EC40F5B3DB',
+            };
+            
+            const main = async () => {
+                request.post({
+                url: 'https://info.gbfteamraid.fun/web/guildrank?method=getGuildrank&params=%7B%22teamraidid%22%3A%22teamraid057%22%2C%22guildid%22%3A%22%22%2C%22guildname%22%3A%22' + args[1] + '%22%7D', 
+                headers: headers,
+            }, (error, response, body) => {
+                    var obj = JSON.parse(response.body);
+                    if (obj.result.length == 0) {
+                        msg.channel.send("Crew tidak ditemukan.");
+                    } else {
+                        var field = new Array();
+                        Object.entries(obj.result).forEach(
+                            ([key, value]) => field[key] = {
+                                'name': "Name: " + value.name,
+                                'value': "ID: " + value.guildid + " | " + "Prelim Rank: " + value.rank,
+                                'inline': true,
+                            }
+                        );
+
+                        const crewinfo = new Discord.MessageEmbed()
+                        .setColor('#0099ff')
+                        .setTitle('**List Pencarian**')
+                        .setURL('https://game.granbluefantasy.jp')
+                        .setAuthor(msg.author.username, 'https://gbf.wiki/images/0/03/Stamp148.png', 'https://game.granbluefantasy.jp')
+                        .setDescription('```Untuk mengecheck detail crew jalankan command: >crew ID id_crew```')
+                        .setThumbnail('https://cdn.discordapp.com/attachments/417293593514999820/881502489789222932/unknown.png')
+                        .addFields(field)
+                        .setTimestamp()
+                        .setFooter('Risu-desu!', 'https://gbf.wiki/images/0/03/Stamp148.png');
+
+                        msg.channel.send(crewinfo);
+                    }
+                })
+            }
+            
+            main()
+        }
+    }
+}
